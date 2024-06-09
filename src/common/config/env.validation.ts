ef9aa46259171729fbb13751a60e792dde79803c
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-09 10:36:20 +0700

    chore: gecko api validation

--- src/common/config/env.validation.ts
+++ src/common/config/env.validation.ts
@@ -4,6 +4,10 @@ export const JoiValidation = Joi.object({
   NODE_ENV: Joi.string()
     .valid('development', 'staging', 'production')
     .default('development'),
-  DATABASE_URL: Joi.string().required(),
   PORT: Joi.number().port().default(3333),
+  DATABASE_URL: Joi.string().required(),
+  COIN_GECKO_ENDPOINT: Joi.string().required(),
+  COIN_GECKO_API_VERSION: Joi.string().required(),
+  COIN_GECKO_API_URL: Joi.string().required(),
+  COIN_GECKO_API_KEY: Joi.string().required(),
 });
