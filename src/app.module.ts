01b3f0fc64b37346ac12949f7440d1bbf85e7f5b
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-09 08:56:20 +0700

    feat: rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,37 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenModule } from './token/token.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [TokenModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short-period',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium-period',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long-period',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+    TokenModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerModule,
+    },
+  ],
 })
 export class AppModule {}
