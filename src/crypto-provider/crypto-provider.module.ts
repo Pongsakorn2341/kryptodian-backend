8572cfb780927dbe6f3906ae86a1b4d38cd4b75c
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-13 16:06:15 +0700

    fix: on delete coin casecad

--- src/crypto-provider/crypto-provider.module.ts
+++ src/crypto-provider/crypto-provider.module.ts
@@ -2,11 +2,13 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { CacheModule } from 'src/cache/cache.module';
 import { CoinMarketCapService } from './coin-marketcap/coin-marketcap.service';
+import { CryptoProviderController } from './crypto-provider.controller';
 import { CryptoProviderService } from './crypto-provider.service';
 import { GeckoService } from './gecko/gecko.service';
 
 @Module({
   imports: [HttpModule, CacheModule],
+  controllers: [CryptoProviderController],
   providers: [CryptoProviderService, GeckoService, CoinMarketCapService],
   exports: [CryptoProviderModule],
 })
